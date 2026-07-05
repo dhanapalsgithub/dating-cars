ca624c7f8b88267b6980a99d174a1e403d0b0cb1
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-07-05 19:14:15 +0530

    update

--- backend/server.py
+++ backend/server.py
@@ -237,8 +237,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
